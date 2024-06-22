c24e0a7ffb220cc768c47279d5d646b13b5bf80f
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-23 00:41:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// write code here
+let zone = prompt(`What zone's ticket are you looking for?`).toUpperCase()
+if(zone.length > 2 || zone.length == 0 || zone == ``){
+  alert(`Please check if you input the zone correctly`)
+}
+else{
+  if(zone == `AA`){
+    alert(`The ticket in this zone is 6,500 THB`)
+  }
+  else if(zone == `BB`){
+    alert(`The ticket in this zone is 5,000 THB`)
+  }
+  else if(zone.charAt(0) == `S`){
+    alert(`The ticket in this zone is 5,500 THB`)
+  }
+  else if(zone.length == 1){
+alert(`The ticket in this zone is 4,000 THB`)
+  }
+}
